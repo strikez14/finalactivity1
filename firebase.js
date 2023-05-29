00c864a38acda035b5a0283042895dc084e84400
David <123925102+strikez14@users.noreply.github.com>
2023-05-29 23:41:21 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmtXAh3P00zWkg5R-3cWG2urWancrI7qY",
+  authDomain: "finalactivity1-34296.firebaseapp.com",
+  databaseURL: "https://finalactivity1-34296-default-rtdb.firebaseio.com",
+  projectId: "finalactivity1-34296",
+  storageBucket: "finalactivity1-34296.appspot.com",
+  messagingSenderId: "890539620347",
+  appId: "1:890539620347:web:e0b18e21826633bf65fd5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+  import index from './finalactivity1/index'
+    export default function home(){
+        return {
+           
+        }
+    }
